d8a40d7869eca65c1e597bb1930cdfb361d48f28
daw2-sorroche23 <sorrocheperezzeus@fpllefia.com>
2023-01-17 19:41:21 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app" class="container">
+    <div class="row">
+      <div class="col-md-12 mt-2">
+        <h1>Usuarios</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+  }
+</script>
